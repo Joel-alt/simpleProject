131f73336f9cf7fe1c2670a3285ed802290c3dfb
Joel <joelgabbai@gmail.com>
2023-06-01 22:35:15 +0200

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,25 @@ import {useState} from "react";
 function App() {
     const [firstNumber, setFirstNumber] = useState(0);
     const [secondNumber, setSecondNumber] = useState(0);
+    const [result, setResult] = useState(0);
 
+    function addNumbers() {
+        const data = {"firstNumber": firstNumber, "secondNumber": secondNumber};
+        fetch('http://localhost:8000/compute', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+            })
+            .then(response => response.json())
+            .then(data => {
+                setResult(data.result)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
     return (
     <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
@@ -11,7 +29,8 @@ function App() {
         <input type="text" value={firstNumber} onChange={(e)=>{setFirstNumber(e.target.value)}}/>
         <h1>Enter your second number</h1>
         <input type="text" value={secondNumber} onChange={(e)=>{setSecondNumber(e.target.value)}}/>
-        <button>Add</button>
+        <button onClick={addNumbers}>Add</button>
+        <h1>Result: {result}</h1>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 8000
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+
+    next();
+});
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/compute', (req, res) => {
+    let data = req.body;
+    const result = data.firstNumber*1 + data.secondNumber*1;
+    const resultObject = { "result": result };
+    res.send(resultObject);
 })
 
 app.listen(port, () => {
